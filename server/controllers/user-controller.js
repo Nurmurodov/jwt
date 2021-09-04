8550bf311eeb8cc7609a5b6306557340d2e4459b
Bahrom  Nurmurodov <nbahrom.1nurmurodov@gmail.com>
2021-09-04 20:05:38 +0500

    first commit

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,61 @@
+const userService = require('../service/user-service')
+
+module.exports.singUp = async (req, res, next) => {
+  try {
+    const {email, password} = req.body
+    const userData = await userService.registration(email, password)
+    res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+    return res.status(200).json({
+      message: 'success',
+      user: userData
+    })
+  } catch (e) {
+    res.status(500).json({
+      message: 'error'
+    })
+    console.log(e)
+  }
+}
+
+module.exports.login = (req, res, next) => {
+  try {
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports.logout = (req, res, next) => {
+  try {
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports.activate = (req, res, next) => {
+  try {
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports.refresh = (req, res, next) => {
+  try {
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports.getUsers = (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: 'success',
+      data: [1, 4, 6]
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
